d1a030efd342fdeff9281bb6db328f69b9496e68
Ève Février <efevrier@csfoy.ca>
2021-05-04 11:54:29 -0400

    ajout de variables dans le fichier utilitaires.css

--- js/toggleFullScreen.js
+++ js/toggleFullScreen.js
@@ -1,35 +1,4 @@
 /* Source: https://www.thewebflash.com/toggling-fullscreen-mode-using-the-html5-fullscreen-api/ */
-/**
- ** HTML **
- * <button class="fullscreen__button">
-      <span class="fullscreen__text screen-reader-only">⇱ FullScreen</span>
-      span class="icon icon--fullscreen" aria-hidden="true"></span>
-   </button>
- ** CSS **
- * .fullscreen {
-            display: none;
-        }
-   @media(min-width: 600px){
-        .fullscreen {
-            display: inline-block;
-            margin: 0.75rem;
-            margin-left: auto;
-        }
-        .icon{
-             display: block;
-             width: 1em;
-             height: 1em;
-             background-size: contain;
-             background-repeat: no-repeat;
-        }
-        .icon--fullscreen{
-             background-image: url("images/fullscreen.svg");
-       }
-       .icon--normalscreen{
-            background-image: url("images/normalscreen.svg");
-       }
-   }
- */
 function toggleFullscreen(elem) {
     elem = elem || document.documentElement;
 
@@ -39,6 +8,7 @@ function toggleFullscreen(elem) {
             document.querySelector('.fullscreen__text').innerHTML = "NormalScreen";
             document.querySelector('.fullscreen__button .icon').classList.toggle("icon--fullscreen", false);
             document.querySelector('.fullscreen__button .icon').classList.toggle("icon--normalscreen", true);
+            localStorage.setItem("fullScreenMode", "on");
         }
     } else {
         if (document.exitFullscreen) {
@@ -46,6 +16,7 @@ function toggleFullscreen(elem) {
             document.querySelector('.fullscreen__text').innerHTML = "FullScreen";
             document.querySelector('.fullscreen__button .icon').classList.toggle("icon--fullscreen", true);
             document.querySelector('.fullscreen__button .icon').classList.toggle("icon--normalscreen", false);
+            localStorage.setItem("fullScreenMode", "off");
         }
     }
 }
@@ -53,3 +24,15 @@ function toggleFullscreen(elem) {
 document.querySelector('.fullscreen__button').addEventListener('click', function () {
     toggleFullscreen();
 });
+window.addEventListener("load", function(){
+    let fullScreenMode = localStorage.getItem("fullScreenMode");
+    // alert(fullScreenMode);
+    if(fullScreenMode == "on"){
+        /*
+        document.querySelector('body').requestFullscreen();
+        document.querySelector('.fullscreen__text').innerHTML = "NormalScreen";
+        document.querySelector('.fullscreen__button .icon').classList.toggle("icon--fullscreen", false);
+        document.querySelector('.fullscreen__button .icon').classList.toggle("icon--normalscreen", true);
+        */
+    }
+})
